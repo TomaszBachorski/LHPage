30f2a948d64639327b41928c4c81018fcbfd3016
TomaszBachorski <tomekbachorski@o2.pl>
2023-06-15 19:20:57 +0200

    main page was developed more

--- src/App.js
+++ src/App.js
@@ -1,16 +1,55 @@
 import React from "react";
 import Box from "./components/Box.js";
+import Clock from "./components/Clock.js";
 
 class App extends React.Component {
     socials = ['Spotify', 'Facebook', 'Messenger', 'GitHub', 'ChatGPT', 'Newtab'];
+    weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    state = {date: new Date()}
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            launchDate: new Date(),
+            date: new Date()
+        };
+    }
+
+    getDate() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    getOrdinalNum(n) {
+        return (n > 0 ? ['th', 'st', 'nd', 'rd'][(n > 3 && n < 21) || n % 10 > 3 ? 0 : n % 10] : '');
+    }
+
+    getDayOfTheYear(){
+        const now = this.state.date
+        let start = new Date(now.getFullYear(), 0, 0);
+        let diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000);
+        let day = Math.floor(diff / (1000 * 60 * 60 * 24));
+        return  day.toString();
+    }
 
     render() {
         return (
             <div className="App">
-                {this.socials.map((social) => (
-                    <Box name={social} key={social} />
-                ))}
+                <div className="Information">
+                    <h1>Hi iThomash</h1>
+                    <h2>Today is {this.weekday[this.state.date.getDay().toString()]} {this.state.date.toLocaleDateString()}</h2>
+                    <h2>Today is <mark className="green">{this.getDayOfTheYear() + this.getOrdinalNum(this.getDayOfTheYear())}</mark> day of the year, <mark className="green">{(this.state.date.getFullYear()%4) ? (365-this.getDayOfTheYear()).toString() : (366-this.getDayOfTheYear()).toString()}</mark> more to go!</h2>
+                    <h2 id="clock"><Clock /></h2>
+                </div>
+                <div id="upcomingEvents">
+                    No upcoming tasks
+                </div>
+                <div className="Boxes">
+                    {this.socials.map((social) => (
+                        <Box name={social} key={social} />
+                    ))}
+                </div>
             </div>
         );
     }
--- src/components/Box.js
+++ src/components/Box.js
@@ -6,34 +6,38 @@ import google from "../assets/Google.png"
 class Box extends React.Component {
     
     tag = null;
-
+    href = "";
     
     render() {
-        console.log(this.props.name);
-
         if (this.props.name === "Spotify") {
             this.tag = <Spotify size={100} color="#1DB954"/>;
+            this.href = "spotify:track";
         }
         if (this.props.name === "Facebook") {
             this.tag = <Facebook size={100} color="#4267B2"/>;
+            this.href = "https://www.facebook.com/";
         }
         if (this.props.name === "Messenger") {
             this.tag = <Messenger size={100} color="#00B2FF"/>;
+            this.href = "https://www.messenger.com/";
         }
         if (this.props.name === "GitHub") {
-            this.tag = <Github size={100} color="#171515"/>;
+            this.tag = <Github size={100} color="#f5f5f5"/>;
+            this.href = "https://github.com/TomaszBachorski";
         }
         if (this.props.name === "ChatGPT") {
             this.tag = <img src={chatgpt} width="100" height="100" alt="chatgpt icon"></img>;
+            this.href = "https://chat.openai.com/";
         }
         if (this.props.name === "Newtab") {
             this.tag = <img src={google} width="100" height="100" alt="google icon"></img>;
+            this.href = "https://www.google.com/"
         }
 
         
         return (
         <div className="IconBox">
-            {this.tag}
+            <a href = {this.href} target="blank" rel="noreferrer">{this.tag}</a>
         </div>)
     }
 }
